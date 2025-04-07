a2c3e8745ce7cfd0a47799200dba4f7384c94cab
Sebastian-Betancourt <168014602+Sebastian-Betancourt@users.noreply.github.com>
2025-04-07 11:41:15 -0500

    Update orden-reparacion.js

--- my-frontend/public/Orden_Reparacion/orden-reparacion.js
+++ my-frontend/public/Orden_Reparacion/orden-reparacion.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let orders = [];
 
   // Realiza la solicitud GET a la API de órdenes de reparación
-  fetch('http://localhost:8084/api/ord_rep/findAll')
+  fetch('https://backend-int-production.up.railway.app/api/ord_rep/findAll')
     .then(response => {
       if (!response.ok) {
         throw new Error('Error en la solicitud: ' + response.status);
