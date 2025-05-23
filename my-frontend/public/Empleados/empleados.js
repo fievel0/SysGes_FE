0f8d12883e08bdf56117d6b525063903b64c93b3
Fabian Paucar <faibol2000@gmail.com>
2025-05-23 16:15:33 -0500

    Update empleados.js

--- my-frontend/public/Empleados/empleados.js
+++ my-frontend/public/Empleados/empleados.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let employees = [];
 
   // Realiza la solicitud GET usando Fetch de forma automática
-  fetch('https://sysgesbe-production.up.railway.app/api/employee/findAll')
+  fetch('https://backend.int-solutionstec.com/api/employee/findAll')
     .then(response => {
       if (!response.ok) {
         throw new Error('Error en la solicitud: ' + response.status);
